02e5f1b86ae939394799edd5f92d96acbbfa249a
YaromirKo <34893823+YaromirKo@users.noreply.github.com>
2020-10-05 19:07:16 +0500

    new sw, assetlinks

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,17 +2,28 @@
 // workbox.precaching.suppressWarnings();
 // workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-let urlsToCache = [
-    '/',
-    '/index.html',
-    '/css/app.90baa6be.css',
-    '/**/*.js',
-    '/**/*.css',
-    '/js/app.3c6ba691.js',
-    '/js/chunk-vendors.27423cc5.js',
-    '/media/audio.6227e272.mp3'
-];
+// let urlsToCache = [
+//     '/',
+//     '/index.html',
+//     '/css/app.90baa6be.css',
+//     '/**/*.js',
+//     '/**/*.css',
+//     '/js/app.3c6ba691.js',
+//     '/js/chunk-vendors.27423cc5.js',
+//     '/media/audio.6227e272.mp3'
+// ];
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+let urlsToCache = '/index.html'
+
 const CACHE_VERSION  = 'v1.0' //Change this value every time before you build
+
+self.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "SKIP_WAITING") {
+        self.skipWaiting();
+    }
+});
 
 self.addEventListener('install', function(event) {
     // Perform install steps
@@ -20,7 +31,7 @@ self.addEventListener('install', function(event) {
         caches.open(CACHE_VERSION)
             .then(function(cache) {
                 console.log('Opened cache');
-                return cache.addAll(urlsToCache);
+                return cache.add(urlsToCache);
             })
     );
 });
@@ -39,16 +50,29 @@ self.addEventListener('activate', function (event) {
     );
 });
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
-        })
-    );
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.mode === 'navigate') {
+        event.respondWith((async () => {
+            try {
+                const preloadResp = await event.preloadResponse;
+
+                if (preloadResp) {
+                    return preloadResp;
+                }
+
+                const networkResp = await fetch(event.request);
+                return networkResp;
+            } catch (error) {
+
+                const cache = await caches.open(CACHE_VERSION);
+                const cachedResp = await cache.match(urlsToCache);
+                return cachedResp;
+            }
+        })());
+    }
 });
 
-// workbox.skipWaiting()
-// workbox.clientsClaim()
 
 self.__precacheManifest = [].concat(self.__precacheManifest || [])
 workbox.precaching.suppressWarnings()
