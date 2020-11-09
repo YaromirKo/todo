c88d9da480295403d4f9ff769147bf7be4fa03b1
YaromirKo <34893823+YaromirKo@users.noreply.github.com>
2020-11-09 22:43:22 +0500

    sw 10

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,8 +2,8 @@
 // workbox.precaching.suppressWarnings();
 // workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-// importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-const CACHE_VERSION  = 'v1.9.11.2020.10' //Change this value every time before you build
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+const CACHE_VERSION  = 'v1.9.11.2020.1' //Change this value every time before you build
 
 self.addEventListener('message', (event) => {
     if (event.data && event.data.type === 'SKIP_WAITING') {
@@ -12,8 +12,8 @@ self.addEventListener('message', (event) => {
 })
 
 
-self.__precacheManifest = [].concat(self.__precacheManifest || [])
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+// self.__precacheManifest = [].concat(self.__precacheManifest || [])
+// workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 
 
 self.addEventListener('install', function(e) {
@@ -21,11 +21,7 @@ self.addEventListener('install', function(e) {
         caches.open(CACHE_VERSION).then(function(cache) {
             return cache.addAll([
                 './',
-                "index.html",
-                "./css/app.14a6d29b.css",
-                "./js/app.6e0b4853.js",
-                "./js/chunk-vendors.e89c98a2.js",
-                "./manifest.json"
+                'index.html'
             ]);
         })
     );
@@ -60,20 +56,11 @@ self.addEventListener("activate", function(event) {
 //     );
 // });
 
-// self.addEventListener("fetch", (event) => {
-//     console.log("[ServiceWorker] Fetch");
-//     event.respondWith(
-//         caches.match(event.request).then(function(response) {
-//             return response || fetch(event.request);
-//         })
-//     );
-//
-// });
 
 self.addEventListener('fetch', function(event) {
     console.log(event.request.url);
 
-    event.respondWith(
+    event.waitUntil(
         caches.match(event.request).then(function(response) {
             return response || fetch(event.request);
         })
@@ -88,45 +75,45 @@ self.addEventListener('fetch', function(event) {
 //     workbox.navigationPreload.enable();
 // }
 console.log(`Workbox is loaded`);
-// if (workbox) {
-//
-//     // const bgSyncPlugin = new workbox.backgroundSync.Plugin(QUEUE_NAME, {
-//     //     maxRetentionTime: 24 * 60 // Retry for max of 24 Hours (specified in minutes)
-//     // });
-//     // const networkOnlyStrategy = new workbox.strategies.NetworkOnly({
-//     //     plugins: [bgSyncPlugin],
-//     // });
-//     // workbox.routing.registerRoute(
-//     //     new RegExp('/*'),
-//     //     new workbox.strategies.StaleWhileRevalidate({
-//     //         cacheName: CACHE_VERSION,
-//     //         plugins: [
-//     //             bgSyncPlugin
-//     //         ]
-//     //     })
-//     // );
-//     workbox.routing.registerRoute(
-//         new RegExp('/*'),
-//         new workbox.strategies.CacheFirst({
-//             cacheName: CACHE_VERSION
-//         })
-//     );
-//
-//     workbox.loadModule('workbox-cacheable-response');
-//     workbox.loadModule('workbox-range-requests');
-//
-//     // workbox.routing.registerRoute(
-//     //     /.*\.mp3/,
-//     //     new workbox.strategies.CacheFirst({
-//     //         cacheName: CACHE_VERSION,
-//     //         plugins: [
-//     //             new workbox.cacheableResponse.CacheableResponsePlugin({statuses: [200]}),
-//     //             new workbox.rangeRequests.RangeRequestsPlugin(),
-//     //         ],
-//     //     }),
-//     // );
-//
-// }
-// else {
-//     console.log(`Workbox didn't load`);
-// }
+if (workbox) {
+
+    // const bgSyncPlugin = new workbox.backgroundSync.Plugin(QUEUE_NAME, {
+    //     maxRetentionTime: 24 * 60 // Retry for max of 24 Hours (specified in minutes)
+    // });
+    // const networkOnlyStrategy = new workbox.strategies.NetworkOnly({
+    //     plugins: [bgSyncPlugin],
+    // });
+    // workbox.routing.registerRoute(
+    //     new RegExp('/*'),
+    //     new workbox.strategies.StaleWhileRevalidate({
+    //         cacheName: CACHE_VERSION,
+    //         plugins: [
+    //             bgSyncPlugin
+    //         ]
+    //     })
+    // );
+    workbox.routing.registerRoute(
+        new RegExp('/*'),
+        new workbox.strategies.CacheFirst({
+            cacheName: CACHE_VERSION
+        })
+    );
+
+    workbox.loadModule('workbox-cacheable-response');
+    workbox.loadModule('workbox-range-requests');
+
+    // workbox.routing.registerRoute(
+    //     /.*\.mp3/,
+    //     new workbox.strategies.CacheFirst({
+    //         cacheName: CACHE_VERSION,
+    //         plugins: [
+    //             new workbox.cacheableResponse.CacheableResponsePlugin({statuses: [200]}),
+    //             new workbox.rangeRequests.RangeRequestsPlugin(),
+    //         ],
+    //     }),
+    // );
+
+}
+else {
+    console.log(`Workbox didn't load`);
+}
